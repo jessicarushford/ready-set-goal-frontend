3a6ee292125a82e2f647a8d1a7cc301e22f12823
Isaiah <isaiah.sweezie@gmail.com>
2022-04-08 17:00:48 -0400

    front-end-additions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,23 @@
 import { Link } from "react-router-dom";
+import { signInWithGoogle, signOut } from "../firebaseConfig";
 import "./Header.css";
 
 const Header = () => {
   return (
     <header className="Header">
-      <Link to="/">
-        <h1>HEADER</h1>
-      </Link>
+      <h1>Ready, Set, Goal</h1>
+      <Link to="/"></Link>
+      <div>
+        <Link to="">
+          <button>My Goals</button>
+        </Link>
+        <button>Previous Goals</button>
+        <button onClick={signInWithGoogle}>Sign In</button>
+        <button onClick={signOut}>Sign Out</button>
+        <Link to="">
+          <button>Friends</button>
+        </Link>
+      </div>
     </header>
   );
 };
--- src/components/HomeRoute.tsx
+++ src/components/HomeRoute.tsx
@@ -1,20 +1,21 @@
 import "./HomeRoute.css";
 import { signInWithGoogle } from "../firebaseConfig";
 import { useNavigate } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import AuthContext from "../context/AuthContext";
 
 // Login + Dashboard (Quote + Cards)
 const HomeRoute = () => {
   const navigate = useNavigate();
-
-  // const dashboardLink = ():void => {
-  //   signInWithGoogle().then(()=> {
-  //     navigate("/dashboard")
-  //   });
-  // };
-
+  const { user } = useContext(AuthContext);
+  useEffect(() => {
+    if (user) {
+      navigate("/dashboard");
+    }
+  }, [user]);
   return (
     <div className="HomeRoute">
-      {/* <button onClick={dashboardLink}>LOGIN</button> */}
+      <button onClick={signInWithGoogle}>LOGIN</button>
     </div>
   );
 };
