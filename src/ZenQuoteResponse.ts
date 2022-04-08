8ed4f6b38e4b38f6ba2e9e640a0b570b7eb9237a
jessicarushford <jessicalynnrushford@gmail.com>
2022-04-08 10:43:08 -0400

    third party api

---
+++ src/ZenQuoteResponse.ts
@@ -0,0 +1,4 @@
+export default interface ZenQuoteResponse {
+  q: string;
+  a: string;
+}
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
+import { getDailyQuote } from "../services/ZenQuotesService";
+import ZenQuoteResponse from "../ZenQuoteResponse";
 import "./Dashboard.css";
 
 // PUT IN HOME ROUTE
 const Dashboard = () => {
-  return <div className="Dashboard">Dashboard works</div>;
+  const [quote, setQuote] = useState<ZenQuoteResponse>();
+
+  useEffect(() => {
+    getDailyQuote().then((response) => {
+      setQuote(response);
+    });
+  }, []);
+
+  return (
+    <div className="Dashboard">
+      <p>{quote!.q}</p>
+      <p>{quote!.a}</p>
+    </div>
+  );
 };
 
 export default Dashboard;
