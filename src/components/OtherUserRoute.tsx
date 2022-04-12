2bace800daca7ef14e4f26a7c2d0feacfdb3a1de
Isaiah <isaiah.sweezie@gmail.com>
2022-04-12 14:42:38 -0400

    otherUserpush

--- src/components/OtherUserRoute.tsx
+++ src/components/OtherUserRoute.tsx
@@ -1,12 +1,17 @@
 import "./OtherUserRoute.css";
 import Goal from "../models/Goal";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import GoalCard from "./GoalCard";
 import { getGoals } from "../services/GoalsService";
 import { useParams } from "react-router-dom";
 import TodaysCard from "./TodaysCard";
-import { addNewFriend, getUserById } from "../services/UserService";
+import {
+  addNewFriend,
+  deleteFriend,
+  getUserById,
+} from "../services/UserService";
 import Friend from "../models/Friend";
+import AuthContext from "../context/AuthContext";
 
 // interface Props {
 //   goal: Goal;
@@ -14,16 +19,24 @@ import Friend from "../models/Friend";
 // Goal Cards of other users (Today's + Past Achieved or Missed)
 const OtherUserRoute = () => {
   const [goals, setGoals] = useState<Goal[]>([]);
-
   const [friends, setFriends] = useState<Friend[]>([]);
-  // const [goal, setGoal] = useState<Goal>();
   const uid: string | undefined = useParams().uid;
+  const { user } = useContext(AuthContext);
 
   const addFriend = (uid: string): void => {
     addNewFriend(uid).then((response) => {
       setFriends(response.friends);
     });
   };
+
+  const removeFriend = (uid: string): void => {
+    deleteFriend(uid).then(() => {
+      setFriends(friends);
+    });
+  };
+
+  const isFriend = (uid: string): boolean =>
+    friends.some((friend) => friend.uid === uid);
 
   useEffect(() => {
     getGoals({ uid }).then((response) => {
@@ -33,9 +46,24 @@ const OtherUserRoute = () => {
   }, [uid]);
   return (
     <div className="OtherUserRoute">
-      <h2>{goals && goals[0].name.toLowerCase()}'s page</h2>
+      {/* <h2>{goals && goals[0].name.toLowerCase()}'s page</h2> */}
       <h3>TODAY'S GOAL</h3>
-      <i className="fa-solid fa-user-group"></i>
+      {user && isFriend(uid!) ? (
+        <i
+          className="fa-solid fa-user"
+          onClick={() => {
+            removeFriend(uid!);
+          }}
+        ></i>
+      ) : (
+        <i
+          className="fa-regular fa-user"
+          onClick={() => {
+            addFriend(uid!);
+          }}
+        ></i>
+      )}
+
       {/* {goals ? <TodaysCard goal={goals[0]} /> : <p>loading</p>} */}
       <h3>PREVIOUS GOALS</h3>
       <button>ACHIEVED</button>
