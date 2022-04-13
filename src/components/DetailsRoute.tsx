39e6a3b52adea47b33401943aef931af225cbdf7
aprhong90 <bboshsh.sy@gmail.com>
2022-04-13 11:03:12 -0400

    done with likes

--- src/components/DetailsRoute.tsx
+++ src/components/DetailsRoute.tsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Goal from "../models/Goal";
-import { addComment, getGoalById } from "../services/GoalsService";
+import {
+  addComment,
+  addUidToLikes,
+  getGoalById,
+  takeOffUidFromLikes,
+} from "../services/GoalsService";
 import CommentForm from "./CommentForm";
 import "./DetailsRoute.css";
 import GoalCard from "./GoalCard";
@@ -11,20 +16,43 @@ const DetailsRoute = () => {
   const [goal, setGoal] = useState<Goal | undefined>();
   const id = useParams().id;
 
+  const getAndSetGoal = (id: string): void => {
+    getGoalById(id).then((response) => {
+      setGoal(response);
+    });
+  };
+
   const addNewComment = (id: string): void => {
     addComment(id);
   };
 
+  const addLike = (userUid: string): void => {
+    addUidToLikes(id!, userUid).then(() => {
+      getAndSetGoal(id!);
+    });
+  };
+
+  const unLike = (userUid: string): void => {
+    takeOffUidFromLikes(id!, userUid).then(() => getAndSetGoal(id!));
+  };
+
   //get a goal
   useEffect(() => {
-    getGoalById(id!).then((response) => {
-      setGoal(response);
-    });
+    getAndSetGoal(id!);
   }, [id]);
 
   return (
     <div className="DetailsRoute">
-      {goal ? <GoalCard goal={goal} key={goal._id} /> : <p>Loading</p>}
+      {goal ? (
+        <GoalCard
+          goal={goal}
+          key={goal._id}
+          onAddLike={addLike}
+          onUnLike={unLike}
+        />
+      ) : (
+        <p>Loading</p>
+      )}
       <ul>
         {goal?.comments?.map((comment) => (
           <li>
