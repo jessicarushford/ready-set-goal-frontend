e15a207e353d6c35db141ec7dc01243e46496498
jessicarushford <jessicalynnrushford@gmail.com>
2022-04-14 14:12:54 -0400

    friend list and previous goals

--- src/components/FriendRoute.tsx
+++ src/components/FriendRoute.tsx
@@ -1,16 +1,12 @@
 import { useContext, useEffect, useState } from "react";
 import AuthContext from "../context/AuthContext";
 import Friend from "../models/Friend";
-import {
-  addNewFriend,
-  deleteFriend,
-  getUserByUid,
-} from "../services/UserService";
+import { deleteFriend, getUserByUid } from "../services/UserService";
 import "./FriendRoute.css";
 
 const FriendRoute = () => {
   const { user } = useContext(AuthContext);
-  const [friends, setFriends] = useState<Friend[]>();
+  const [friends, setFriends] = useState<Friend[]>([]);
 
   const getAndSetFriends = (userUid: string): void => {
     getUserByUid(userUid).then((response) => {
@@ -19,16 +15,34 @@ const FriendRoute = () => {
     });
   };
 
+  const removeFriend = (userUid: string, otherUserUid: string): void => {
+    deleteFriend(userUid, otherUserUid).then(() => {
+      getAndSetFriends(userUid);
+    });
+  };
+
   useEffect(() => {
     if (user) {
       getAndSetFriends(user.uid);
     }
-  }, [user?.uid]);
+  }, [user]);
 
   return (
     <div className="FriendRoute">
-      {/* <h2>{user && `${user.displayName}`}'s friend list</h2> */}
-      {/* {user && friends!.map((friend) => friend.name)} */}
+      <h2>{user && `${user.displayName?.toLowerCase()}`}'s friend list</h2>
+      <ul>
+        {friends.map((friend) => (
+          <li>
+            {friend.name}
+            <i
+              className="fa-solid fa-user"
+              onClick={() => {
+                removeFriend(user!.uid, friend.uid!);
+              }}
+            ></i>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
--- src/components/PreviousGoalsRoute.tsx
+++ src/components/PreviousGoalsRoute.tsx
@@ -1,31 +1,84 @@
-import { useContext } from "react";
+import { FormEvent, useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
+import Goal from "../models/Goal";
+import QueryStringParams from "../models/QueryStringParams";
+import { getGoals } from "../services/GoalsService";
 import GoalCard from "./GoalCard";
 import "./PreviousGoalsRoute.css";
 
 const PreviousGoalsRoute = () => {
   const { user } = useContext(AuthContext);
+  const [goals, setGoals] = useState<Goal[]>([]);
+  const [achieved, setAchieved] = useState(true);
+  const [todaysGoal, setTodaysGoal] = useState<Goal | undefined>();
+  const newDate: Date = new Date();
+  const year = newDate.getFullYear();
+  const month = newDate.getMonth() + 1;
+  const date = newDate.getDate();
+  const fullDate = `${month}.${date}.${year}`;
+  const userUid: string | undefined = useParams().uid;
+  const params: QueryStringParams = {
+    uid: userUid,
+  };
+  const [keyword, setKeyword] = useState("");
+  const [category, setCategory] = useState("");
+  const [filterDate, setFilterDate] = useState("");
+
+  const submitHandler = (e: FormEvent): void => {
+    const params: any = {
+      ...(keyword ? { keyword } : {}),
+      ...(category ? { category } : {}),
+      ...(filterDate ? { filterDate } : {}),
+    };
+    goals
+      .filter((goal) => goal.category === goal.category)
+      .map((goal) => {
+        <GoalCard key={goal._id} goal={goal} />;
+      });
+
+    setKeyword("Search");
+    setCategory("");
+    setFilterDate("");
+  };
+
+  useEffect(() => {
+    getGoals(params).then((response) => {
+      console.log(response);
+      setGoals(response);
+      const todaysgoal = response.find((goal) => goal.date === fullDate);
+      setTodaysGoal(todaysgoal);
+    });
+  }, []);
 
   return (
     <div className="PreviousGoalsRoute">
       <h2>{user?.displayName?.toLowerCase()}'s goals</h2>
       <h3>PREVIOUS GOALS</h3>
-      <button>ACHIEVED</button>
-      <button>MISSED</button>
-      <form>
+      <form onSubmit={submitHandler}>
         <div>
-          <i className="fa-solid fa-magnifying-glass"></i>
           <input
             type="text"
             className="SearchBar"
             placeholder="Search"
             name="SearchBar"
             id="SearchBar"
+            onChange={(e) => setKeyword(e.target.value)}
+            value={keyword}
           ></input>
+          <i
+            className="fa-solid fa-magnifying-glass"
+            onClick={submitHandler}
+          ></i>
         </div>
 
         <div>
-          <select name="category" id="category">
+          <select
+            name="category"
+            id="category"
+            onChange={(e) => setCategory(e.target.value)}
+            value={category}
+          >
             <option value="" disabled hidden>
               Category
             </option>
@@ -42,6 +95,15 @@ const PreviousGoalsRoute = () => {
         {/* <button></button>SUBMIT BUTTON? */}
         <div>{/* <GoalCard /> MAP THRU GOAL CARDS OF SEARCH */}</div>
       </form>
+      <button onClick={() => setAchieved(true)}>ACHIEVED</button>
+      <button onClick={() => setAchieved(false)}>MISSED</button>
+      <ul>
+        {goals
+          .filter((goal) => goal.completed === achieved && goal !== todaysGoal)
+          .map((goal) => (
+            <GoalCard key={goal._id} goal={goal} />
+          ))}
+      </ul>
     </div>
   );
 };
