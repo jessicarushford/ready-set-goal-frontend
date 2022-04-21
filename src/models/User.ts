d39eaab65f78dcbd2f38cff5e54f353d36136237
aprhong90 <bboshsh.sy@gmail.com>
2022-04-21 10:43:07 -0400

    lastLogin

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,9 +1,10 @@
 import Friend from "./Friend";
+import LastLogin from "./LastLogin";
 
 export default interface User {
   _id?: string;
   uid: string;
   name: string;
   friends: Friend[];
-  lastLogin: string;
+  lastLogin: LastLogin;
 }
